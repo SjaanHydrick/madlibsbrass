2e6a648086edb263cc64da6a8ca034dc7000219c
Shane Upchurch <shaneupchurch@gmail.com>
2020-09-25 18:41:24 -0500

    adds const in js

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+const geoInput = document.getElementById("geographic-feature-input");
+const placeNameInput = document.getElementById("place-name-input");
+const nameInput = document.getElementById("name-input");
+const noun1Input = document.getElementById("noun1-input");
+const noun2Input = document.getElementById("noun2-input");
+const adjInput = document.getElementById("adjective-input");
+const verbInput = document.getElementById("verb-input");
+const animalInput = document.getElementById("animal-input");
+const button = document.getElementById("button");
+
+
+
+button.addEventListener('click', () => {
+
+}
+)
